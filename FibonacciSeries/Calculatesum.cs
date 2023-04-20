a138199ad7c7483aeafb0d0442fa26c2e6a8b265
BhagyashriSabale <balisabale16@gmail.com>
2023-04-20 17:08:18 +0530

    [Bhagyashri] calculate fibonacci sum

---
+++ FibonacciSeries/Calculatesum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming.FibonacciSeries
+{
+    internal class Calculatesum
+    {
+        public static void seriesaddition()
+        {
+            int n, i, num1 = 0, num2 = 1, sum = 0;
+
+            Console.WriteLine("Enter the no.of Terms:");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci series:");
+
+            for (i = 1; i <= n; i++)
+            {
+
+                if (i == 1)
+                {
+                    Console.Write(num1 + " ");
+                    continue;
+                }
+                if (i == 2)
+                {
+                    Console.Write(num2 + " ");
+                    continue;
+                }
+                sum = num1 + num2;
+                num1 = num2;
+                num2 = sum;
+                Console.Write(sum + " ");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Dynamic;
+﻿using LogicalProgramming.FibonacciSeries;
+using System.Dynamic;
 
 namespace LogicalProgramming
 {
@@ -7,7 +8,17 @@ namespace LogicalProgramming
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Logical Programming");
-           
+            Console.WriteLine("Please choose 1 Program from below list");
+            Console.WriteLine("1.FibonacciSeries");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+                {
+                case 1:
+                    Calculatesum calculatesum = new Calculatesum();
+                    Calculatesum.seriesaddition();
+                break;
+            }
         }
     }
 }
