33b8169012a0eaf012c738ad6d36ff7d27bae685
BhagyashriSabale <balisabale16@gmail.com>
2023-04-23 15:33:54 +0530

    [Bhagyashri] Calculate DayofWeek

---
+++ DayofWeek/Util.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming.DayofWeek
+{
+    internal class Util
+    {
+        public static int DayOfWeek(int m, int d, int y)
+        {
+            if (m < 1 || m > 12 || d < 1 || d > 31 || y < 0)
+            {
+                throw new ArgumentException("Invalid input values");
+            }
+
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2;
+            int d0 = (d + x + 31 * m0 / 12) % 7;
+            return d0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using LogicalProgramming.FibonacciSeries;
+﻿using LogicalProgramming.DayofWeek;
+using LogicalProgramming.FibonacciSeries;
 using LogicalProgramming.PerfectNumber;
 using LogicalProgramming.PrimeNumber;
 using LogicalProgramming.ReverseNumber;
@@ -14,6 +15,8 @@ namespace LogicalProgramming
             Console.WriteLine("Welcome Logical Programming");
             Console.WriteLine("Please choose 1 Program from below list");
             Console.WriteLine("1.FibonacciSeries\n2.PerfectNumber\n3.PrimeNumber\n4.ReverseNumber\n6.Stopwatch");
+
+            Console.WriteLine("8.DayofWeek");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -38,6 +41,29 @@ namespace LogicalProgramming
                      ElapsedTime elapsedTime = new ElapsedTime();
                      ElapsedTime.Time();
                 break;
+
+                case 8:
+                    try
+                    {
+                        Console.WriteLine("Enter value of m: ");
+                        int m = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter value of d: ");
+                        int d = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter value of y: ");
+                        int y = int.Parse(Console.ReadLine());
+
+                        int dayOfWeek = Util.DayOfWeek(m, d, y);
+
+                        Console.WriteLine(dayOfWeek);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine("Error: " + e.Message);
+                    }
+
+                    break;
             }
         }
     }
