02874c170844841e22720c0709ceba47a47dbd28
BhagyashriSabale <balisabale16@gmail.com>
2023-04-23 15:37:16 +0530

    [Bhagyashri] Calculate DayofWeek

--- DayofWeek/Util.cs
+++ DayofWeek/Util.cs
@@ -10,10 +10,7 @@ namespace LogicalProgramming.DayofWeek
     {
         public static int DayOfWeek(int m, int d, int y)
         {
-            if (m < 1 || m > 12 || d < 1 || d > 31 || y < 0)
-            {
-                throw new ArgumentException("Invalid input values");
-            }
+          
 
             int y0 = y - (14 - m) / 12;
             int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
